c3a9366b1315e8ee4da374f4ae60223901101a91
hetjpatel55 <hpatel55@student.cscc.edu>
2025-07-21 13:02:14 -0400

    added alien vertically

--- alien_fleet.py
+++ alien_fleet.py
@@ -5,6 +5,7 @@ from alien import Alien
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
 
+
 class AlienFleet:
     def __init__(self, game: "AlienInvasion"):
         self.game = game
@@ -17,32 +18,44 @@ class AlienFleet:
 
     def create_fleet(self):
         alien_w = self.settings.alien_w
+        alien_h = self.settings.alien_h
         screen_w = self.settings.screen_w
+        screen_h = self.settings.screen_h
+        fleet_w, fleet_h = self.calculate_fleet_size(
+            alien_w, screen_w, alien_h, screen_h
+        )
 
-        fleet_w = self.calculate_fleet_size(screen_w, alien_w)
-
-        # half_screen = self.screen_w
+        half_screen = screen_h // 2
         fleet_horizontal_space = fleet_w * alien_w
-        x_offset = int((screen_w- fleet_horizontal_space)//2)
+        fleet_verticle_space = fleet_h * alien_h
+        x_offset = int((screen_w - fleet_horizontal_space) // 2)
+        y_offset = int((half_screen - fleet_verticle_space) // 2)
+        for row in range(fleet_h):
+            for col in range(fleet_w):
+                current_x = alien_w * col + x_offset
+                current_y = alien_h * row + y_offset
+                if col % 2 == 0 or row % 2 ==0:
+                    continue
+                self._create_alien(current_x, current_y)
 
-        for col in range(fleet_w):
-            current_x = alien_w * col + x_offset
-            if col % 2 == 0:
-                continue
-            self._create_alien(current_x, 10)     
-
-    def calculate_fleet_size(self,screen_w, alien_w):
+    def calculate_fleet_size(self, alien_w, screen_w, alien_h, screen_h):
         fleet_w = screen_w // alien_w
+        fleet_h = (screen_h / 2) // alien_h
 
         if fleet_w % 2 == 0:
             fleet_w -= 1
         else:
             fleet_w -= 2
 
-        return fleet_w
+        if fleet_h % 2 == 0:
+            fleet_h -= 1
+        else:
+            fleet_h -= 2
+
+        return int(fleet_w), int(fleet_h)
 
     def _create_alien(self, current_x: int, current_y: int):
-        new_alien = Alien(self, current_x,10)
+        new_alien = Alien(self, current_x, current_y)
         self.fleet.add(new_alien)
 
     def draw(self):
