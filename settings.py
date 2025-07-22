fca268896bf9e1c489b2caa190ef2432c39acd37
hetjpatel55 <hpatel55@student.cscc.edu>
2025-07-22 17:43:55 -0400

    unit 11 homework

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
             / "unit_11_alien_invasion_starter"
             / "Assets"
             / "images"
-            / "Starbasesnow.png"
+            / "Starbasesnow-removebg.png"
         )
 
         self.ship_file = (
@@ -21,22 +21,17 @@ class Settings:
             / "unit_11_alien_invasion_starter"
             / "Assets"
             / "images"
-            / "ship2(no bg).png"
+            / "ship-removebg.png"
         )
         self.ship_w = 40
         self.ship_h = 60
         self.ship_speed = 5
 
-        self.bullet_file = Path.cwd() / "unit_11_alien_invasion_starter" / "Assets" / "images" / "laserBlast.png"
+        self.bullet_file = Path.cwd() / "unit_11_alien_invasion_starter" / "Assets" / "images" / "laserBlast-removebg.png"
+
         self.laser_sound = Path.cwd() / "unit_11_alien_invasion_starter" / "Assets" / "sound" / "laser.mp3"
+
         self.bullet_speed = 7
         self.bullet_w = 25
         self.bullet_h = 80
         self.bullet_amount = 5
-
-        self.alien_file = (Path.cwd()/ "unit_11_alien_invasion_starter"/ "Assets"/ "images"/ "enemy_4.png")
-        self.alien_w = 40
-        self.alien_h = 40
-        self.fleet_speed = 2
-        self.fleet_direction = 1
-        self.fleet_drop_speed = 40
