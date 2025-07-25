6aac298a2b085ae572d72fded7ee404d588351a9
hetjpatel55 <hpatel55@student.cscc.edu>
2025-07-24 20:56:09 -0400

    added difficulty to the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,6 +13,7 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
+        self.settings.initialize_dynamic_settings()
         self.game_stats = GameStats(self.settings.starting_ship_amount)
 
         self.screen = pygame.display.set_mode(
@@ -64,6 +65,8 @@ class AlienInvasion:
 
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
+            self.settings.increase_difficulty()
+
 
     def _check_game_status(self):
         if self.game_stats.ships_left > 0:
@@ -80,7 +83,7 @@ class AlienInvasion:
         self.alien_fleet.create_fleet()
     
     def restart_game(self):
-
+        self.settings.initialize_dynamic_settings()
         self._reset_level()
         self.ship._center_ship()
         self.game_active = True
--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
             / "images"
             / "Starbasesnow.png"
         )
-
+        self.difficulty_scale = 1.1
         self.ship_file = (
             Path.cwd()
             / "unit_11_alien_invasion_starter"
@@ -25,24 +25,15 @@ class Settings:
         )
         self.ship_w = 40
         self.ship_h = 60
-        self.ship_speed = 5
-        self.starting_ship_amount = 3
 
         self.bullet_file = Path.cwd() / "unit_11_alien_invasion_starter" / "Assets" / "images" / "laserBlast.png"
         self.laser_sound = Path.cwd() / "unit_11_alien_invasion_starter" / "Assets" / "sound" / "laser.mp3"
         self.impact_sound = (Path.cwd()/ "unit_11_alien_invasion_starter"/ "Assets"/ "sound"/ "impactSound.mp3"
         )
-        self.bullet_speed = 7
-        self.bullet_w = 25
-        self.bullet_h = 80
-        self.bullet_amount = 5
-
         self.alien_file = (Path.cwd()/ "unit_11_alien_invasion_starter"/ "Assets"/ "images"/ "enemy_4.png")
         self.alien_w = 40
         self.alien_h = 40
-        self.fleet_speed = 2
         self.fleet_direction = 1
-        self.fleet_drop_speed = 40
 
         self.button_w = 200
         self.button_h = 50
@@ -52,4 +43,20 @@ class Settings:
         self.button_font_size = 48
         self.HUD_font_size = 20
         self.font_file = Path.cwd()/ 'unit_11_alien_Invasion_starter'/ 'Assets' / 'Fonts' / 'Silkscreen' / 'Silkscreen-Bold.ttf'
-        
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 5
+        self.starting_ship_amount = 3
+
+        self.bullet_w = 25
+        self.bullet_h = 80
+        self.bullet_speed = 7
+        self.bullet_amount = 5
+
+        self.fleet_speed = 2
+        self.fleet_drop_speed = 40
+
+    def increase_difficulty(self):
+        self.ship_speed *= self.difficulty_scale
+        self.bullet_speed *= self.difficulty_scale
+        self.fleet_speed *= self.difficulty_scale
